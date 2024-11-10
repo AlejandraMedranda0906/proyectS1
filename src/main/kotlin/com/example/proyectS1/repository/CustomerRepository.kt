a6408d69daf0dcda81a5a42476c7c96fe28953f5
Alejandra <146900779+AlejandraMedranda0906@users.noreply.github.com>
2024-11-10 13:06:51 -0500

    .

--- src/main/kotlin/com/example/proyectS1/repository/CustomerRepository.kt
+++ src/main/kotlin/com/example/proyectS1/repository/CustomerRepository.kt
@@ -1,11 +1,17 @@
 package com.example.proyectS1.repository
 
+import com.example.proyectS1.entity.Attention
 import com.example.proyectS1.entity.Customer
+import com.example.proyectS1.entity.Professional
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface CustomerRepository:JpaRepository<Customer, Long>{
+    interface ProfessionalRepository : JpaRepository<Professional, Long>
+    interface AttentionRepository : JpaRepository<Attention, Long>
+
+
 
 
 }
