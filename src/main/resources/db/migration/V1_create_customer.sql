b68126ba88231f1178aa217c70c2c79bc5e5f3e1
Alejandra <146900779+AlejandraMedranda0906@users.noreply.github.com>
2024-11-10 08:53:11 -0500

    .

--- src/main/resources/db/migration/V1_create_customer.sql
+++ src/main/resources/db/migration/V1_create_customer.sql
@@ -9,5 +9,41 @@ CREATE TABLE IF NOT EXISTS client(
     PRIMARY KEY(id)
     );
 
+CREATE TABLE IF NOT EXISTS Professional (
+      id SERIAL,
+      full_name VARCHAR(100) NOT NULL,
+      especialidad VARCHAR(100),
+      email VARCHAR(100) UNIQUE
+      PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS Attention(
+    id SERIAL PRIMARY KEY,
+    client_id INT NOT NULL,
+    Professional_id INT NOT NULL,
+    fecha DATE NOT NULL,
+    descripcion TEXT,
+    FOREIGN KEY (client_id) REFERENCES Client(id),
+    FOREIGN KEY (Professional_id) REFERENCES Professional(id)
+    );
+
+
+
+
+
+
+
+CREATE TABLE IF NOT EXISTS Atencion (
+
+    id SERIAL PRIMARY KEY,
+
+    cliente_id INT NOT NULL,
+    profesional_id INT NOT NULL,
+    phone VARCHAR NOT NULL,
+    descripcion TEXT, -- Descripción del tipo de ayuda o consulta
+    FOREIGN KEY (cliente_id) REFERENCES Cliente(id),
+    FOREIGN KEY (profesional_id) REFERENCES Profesional(id)
+    );
+
 
 
